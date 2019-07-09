ceb1e79902fb08fb017e769a89011a67e90bacbf
immutable-mixer-9366 <natcunanan@gmail.com>
2019-07-09 00:37:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  if (string === string.lowercase){
+  if (string.lowercase() === string){
     return "I can\'t hear you!"
   }
   else {
